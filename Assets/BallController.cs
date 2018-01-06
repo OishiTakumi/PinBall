da17a024265da0dafeabfa7151f9b34b8c77334e
OishiTakumi <wing_guts55@yahoo.co.jp>
2018-01-07 02:07:20 +0900

    Lesson5 3Dゲームアプリを作ろう Vol.1
    課題：得点を表示しよう
    発展課題：スマートフォンでも動かせるようにマルチタッチに対応しましょう

---
+++ Assets/BallController.cs
@@ -0,0 +1,118 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;//UIをスクリプトから扱えるように
+
+public class BallController : MonoBehaviour
+{
+    //ボールが見える可能性のあるz軸の最大値
+    private float visiblePosZ = -6.5f;
+
+    //ゲームオーバを表示するテキスト
+    private GameObject gameoverText;
+  
+    public Text ScoreLabel;//スコア―を表示するテキストUI格納
+
+    public Text ScoreText; // スコアを表示する
+
+    public Text HighScoreText;// ハイスコアを表示する
+
+    // スコア
+    private int Score;
+
+    // ハイスコア
+    private int HighScore;
+
+    // PlayerPrefsで保存するためのキー
+    private string HighScoreKey = "HighScore";
+   
+
+    void Start() // Use this for initialization
+    {
+        Initialize();
+        //シーン中のGameOverTextオブジェクトを取得
+        this.gameoverText = GameObject.Find("GameOverText");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //ScoreLabel.text = "スコア―:" + Score;//スコア―を表示 
+        
+        // スコアがハイスコアより大きければ
+        if (HighScore < Score)
+        {
+            HighScore = Score;
+        }
+
+        // スコア・ハイスコアを表示する
+        ScoreText.text = Score.ToString();
+        HighScoreText.text = HighScore.ToString();
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+
+        if (other.gameObject.tag == "SmallCloudTag")
+        {
+            UpdateScore(10);
+        }
+        else if (other.gameObject.tag == "LargeCloudTag")
+        {
+            UpdateScore(20);  //大きい雲と衝突した場合の処理
+        }
+        else if (other.gameObject.tag == "LargeStarTag")
+        {
+            UpdateScore(30);  //大きい星と衝突した場合の処理
+        }
+
+        //ボールが画面外に出た場合
+        if (this.transform.position.z < this.visiblePosZ)
+        {
+            //GameoverTextにゲームオーバを表示
+            this.gameoverText.GetComponent<Text>().text = "Game Over";
+        }
+    }
+
+
+    // ゲーム開始前の状態に戻す
+    private void Initialize()
+    {
+        // スコアを0に戻す
+        Score = 0;
+
+        // ハイスコアを取得する。保存されてなければ0を取得する。
+        HighScore = PlayerPrefs.GetInt(HighScoreKey, 0);
+    }
+
+    // ポイントの追加
+    public void AddPoint(int point)
+    {
+        Score = Score + point;
+    }
+
+    void UpdateScore(int score)
+    {
+        Score += score;//小さい雲と衝突した場合の処理
+
+        // スコアがハイスコアより大きければ
+        if (HighScore < Score)
+        {
+            HighScore = Score;
+        }
+        // スコア・ハイスコアを表示する
+        ScoreText.text = Score.ToString();
+        HighScoreText.text = HighScore.ToString();
+    }
+
+    // ハイスコアの保存
+    public void Save()
+    {
+        // ハイスコアを保存する
+        PlayerPrefs.SetInt(HighScoreKey, HighScore);
+        PlayerPrefs.Save();
+
+        // ゲーム開始前の状態に戻す
+        Initialize();
+    }
+}
+
---
+++ Assets/FripperController.cs
@@ -0,0 +1,109 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FripperController : MonoBehaviour {
+    //HingiJointコンポーネントを入れる
+    private HingeJoint myHingeJoint;
+
+    //初期の傾き
+    private float defaultAngle = 20;
+    //弾いた時の傾き
+    private float flickAngle = -20;
+
+    // Use this for initialization
+    void Start () {
+       
+        //HingeJointコンポーネント取得
+        this.myHingeJoint = GetComponent<HingeJoint>();
+
+        //フリッパーの傾きを設定
+        SetAngle(this.defaultAngle);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //左矢印キーを押した時左フリッパーを動かす
+        if (Input.GetKeyDown(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+        //右矢印キーを押した時右フリッパーを動かす
+        if (Input.GetKeyDown(KeyCode.RightArrow) && tag == "RightFripperTag")
+        {
+            SetAngle(this.flickAngle);
+        }
+
+        //矢印キー離された時フリッパーを元に戻す
+        if (Input.GetKeyUp(KeyCode.LeftArrow) && tag == "LeftFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+        if (Input.GetKeyUp(KeyCode.RightArrow) && tag == "RightFripperTag")
+        {
+            SetAngle(this.defaultAngle);
+        }
+
+
+        //全てのタッチの情報をfor文で取り出す
+        for (int i = 0; i < Input.touches.Length; i++)
+        {
+
+            //配列の中のi番目のタッチ情報を取り出している
+            Touch t = Input.GetTouch(i);
+
+            //タッチ位置がを取り出す
+            Vector2 touchPos = t.position;
+
+            //画面の半分の横幅をscreenHalfWidth変数に設定
+            float screenHalfWidth = Screen.width / 2; ;
+
+            if (t.phase == TouchPhase.Began)
+            {
+                //タッチしたとき
+                if (screenHalfWidth < touchPos.x && tag == "RightFripperTag")//タッチをした瞬間に行う処理,右のフリッパーを動かす
+                {
+                    {
+                        SetAngle(this.flickAngle);
+                    }
+                }
+
+                if (screenHalfWidth > touchPos.x && tag == "LeftFripperTag")//タッチをした瞬間に行う処理,左のフリッパーを動かす
+                {
+                    {
+                        SetAngle(this.flickAngle);
+                    }
+
+                }
+            }
+
+            else if (t.phase == TouchPhase.Ended)
+            {
+                if (screenHalfWidth < touchPos.x && tag == "RightFripperTag")　//タッチを外した瞬間に行う処理,右のフリッパーを元に戻す
+                {
+                    {
+                        SetAngle(this.defaultAngle);
+                    }
+                }
+
+                if (screenHalfWidth > touchPos.x && tag == "LeftFripperTag") //タッチを外した瞬間に行う処理,左のフリッパーを元に戻す
+                {
+                    {
+                        SetAngle(this.defaultAngle);
+                    }
+
+                }
+            }
+        }
+    }
+       
+    //フリッパーの傾きを設定
+    public void SetAngle(float angle)
+    {
+          JointSpring jointSpr = this.myHingeJoint.spring;
+          jointSpr.targetPosition = angle;
+          this.myHingeJoint.spring = jointSpr;
+    }
+    
+}
